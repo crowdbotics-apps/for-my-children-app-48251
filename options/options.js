f4c0295315f1664ee0464c4f15ff1e0b3ef88b41
Crowdbotics <team@crowdbotics.com>
2024-06-04 15:01:19 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "for-my-children-app-48251",
+  url: "https://for-my-children-app-48251.botics.co",
+  api: "https://for-my-children-app-48251.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
